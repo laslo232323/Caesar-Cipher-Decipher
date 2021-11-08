d1db710d0d178dc5ea48a910db788ce0de9586a2
laslo <laslo232323@protonmail.com>
2021-11-08 17:35:15 +0200

    Update Caesar Cipher (Shift).py

--- Caesar Cipher (Shift).py
+++ Caesar Cipher (Shift).py
@@ -1,13 +1,4 @@
-print('Hey! Welcome to the Caesar Cipher.')
-text = input('Your text: ')
-side_shift = input('Choose your side to shift, left/right: ')
-while side_shift != 'left' and side_shift != 'Left' and side_shift != 'right' and side_shift != 'Right':
-    side_shift = input('Please try again, left/right: ')
-number_shift = int(input('Choose your shift from 1 to 25: '))
-while 1 < number_shift > 25:
-    number_shift = int(input('Please try again, your shift should be 1 to 25: '))
-
-def change_word():
+def change_word_eng():
     if side_shift == 'right' or side_shift == 'Right':
         for c in text:
             flag = False
@@ -58,8 +49,21 @@ def change_word():
                 if flag == False:
                     print(chr(ord(c) - number_shift), end='')
             if c in '!@#$%^&*()-_+=}{[]\~`<>,./?}{: "':
-                print(c, end='')    
+                print(c, end='')
 
-change_word()
-
-
+more = 'y'
+while more == 'y':
+    print('Hey! Welcome to the Caesar Cipher.')
+    text = input('Your text: ')
+    side_shift = input('Choose your side to shift, left/right: ')
+    while side_shift != 'left' and side_shift != 'Left' and side_shift != 'right' and side_shift != 'Right':
+        side_shift = input('Please try again, left/right: ')
+    number_shift = int(input('Choose your shift from 1 to 25: '))
+    while 1 < number_shift > 25:
+        number_shift = int(input('Please try again, your shift should be 1 to 25: '))
+    more = 'n'
+    
+    print('Your Caesar Cipher: ', end='')
+    change_word_eng()
+    print()
+    more = input('More? y/n: ')
