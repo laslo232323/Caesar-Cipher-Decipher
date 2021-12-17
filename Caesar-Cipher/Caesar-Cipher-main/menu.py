61ec69eb7087b67f64d312ca49bbe38ad3521988
laslo <laslo232323@protonmail.com>
2021-12-17 17:07:51 +0200

    Update menu.py
    Without Decipher, worked now. Update menu, use methods, classes. Delete languages.py

--- Caesar-Cipher/Caesar-Cipher-main/menu.py
+++ Caesar-Cipher/Caesar-Cipher-main/menu.py
@@ -1,17 +1,91 @@
-from languages import Language
 from utils import Logic
 
-# MENU
-flag = True
-while flag != False:
-    language = (input('Choose your language, ua/eng/ru: '))
-    flag = False
+class Menu_base:
+    """Main menu"""
 
-if language == 'ru':
-    Language.ru()
+    """Menu where you chose cipher or decipher"""
+    def cipher_decipher_menu():
+        global cipher_decipher
+        cipher_decipher = input('Cipher[1]/Decipher[2]: ')
+        Menu_base.language_input()
 
-if language == 'ua':
-    Language.ua()
+    """Menu where you chose your language main menu, and space work of cipher/decipher"""
+    def language_input():
+        global language
+        language = (input('Ua[1]/Eng[2]/Ru[3]: '))
+        Menu_base.menu()
 
-if language == 'eng':
-    Language.eng()
+    """Main menu on your language"""
+    def menu():
+        """1 = cipher, 2 = decipher"""
+        if cipher_decipher == '1' and '2':
+            """Russian start and end menu"""
+            if language == '3' and cipher_decipher == '1':
+                more = '1'
+                while more == '1':
+                    print('Добро пожаловать в шифр Цезаря!')
+                    Logic.ru_data()
+                    more = input('Еще? да[1]/выход[2]/меню[3]: ')
+                    if more == '3':
+                        Menu_base.cipher_decipher_menu()
+                    if more == '2':
+                        Menu_base.exit()
+                    while more != '1' and more != '2' and more != '3':
+                        more = input('Попробуй еще, да[1]/выход[2]/меню[3]: ')
+                    if more == '1':
+                        Menu_base.menu()
+            """Ukrainian start and end menu"""
+            if language == '1' and cipher_decipher == '1':
+                more = '1'
+                while more == '1':
+                    print('Вітаю, ласкаво просимо до шифру Цезаря!')
+                    Logic.ua_data()
+                    more = input('Ще? так[1]/завершити[2]/меню[3]: ')
+                    if more == '3':
+                        Menu_base.cipher_decipher_menu()
+                    if more == '2':
+                        Menu_base.exit()
+                    while more != '1' and more != '2' and more != '3':
+                        more = input('Спробуй ще, так[1]/завершити[2]/меню[3]: ')
+                    if more == '1':
+                        Menu_base.menu()
+            """English start and end menu"""
+            if language == '2' and cipher_decipher == '1':
+                more = '1'
+                while more == '1':
+                    print('Hey! Welcome to the Caesar Cipher.')
+                    Logic.eng_data()
+                    more = input('More? yes[1]/exit[2]/menu[3]: ')
+                    if more == '3':
+                        Menu_base.cipher_decipher_menu()
+                    if more == '2':
+                        Menu_base.exit()
+                    while more != '1' and more != '2' and more != '3':
+                        more = input('Please try again, yes[1]/exit[2]/menu[3]: ')
+                    if more == '1':
+                        Menu_base.menu()
+
+            # else:
+                # if language == '3' and cipher_decipher == '2':
+                    # Language.ru_decipher()
+                # if language == '1' and cipher_decipher == '2':
+                    # Language.ua_decipher()
+                # if language == '2' and cipher_decipher == '2':
+                   #  Language.eng_decipher()
+
+    """Exit program"""
+    def exit():
+        """Farewell text depending on the selected language"""
+        if language == '2':
+            print('Good bye!')
+        if language == '3':
+            print('Пока!')
+        if language == '1':
+            print('До побачення!')
+        raise SystemExit
+
+
+# Start program
+Menu_base.cipher_decipher_menu()
+
+
